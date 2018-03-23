35263b8f4cdf8bf168d4e1ee08efec5ff17b533a
Aurorayqz <aurorayqz@foxmail.com>
2018-03-24 00:13:23 +0800

    mmap

---
+++ mmap/mmap_mem.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/file.h>
+#include <wait.h>
+#include <sys/mman.h>
+
+#define COUNT 100
+#define MEMSIZE 1024*1024*1023*2
+
+int main()
+{
+    pid_t pid;
+    int count;
+    void *shm_p;
+
+    shm_p = mmap(NULL, MEMSIZE, PROT_WRITE|PROT_READ, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+    if (MAP_FAILED == shm_p) {
+        perror("mmap()");
+        exit(1);
+    }
+
+    bzero(shm_p, MEMSIZE);
+
+    sleep(3000);
+
+    munmap(shm_p, MEMSIZE);
+    exit(0);
+}
